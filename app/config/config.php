e0cd7d68cc754b981c39b7705da4b35abeca11e4
Amaury <amaury.denoyelle@viacesi.fr>
2011-03-23 16:51:54 +0100

    Standard version of config file

--- app/config/config.php
+++ app/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= "http://localhost.cep-exia/";
+$config['base_url']	= "http://localhost/Jexia/";
 
 /*
 |--------------------------------------------------------------------------
